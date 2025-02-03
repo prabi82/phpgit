9192c2bdab9be61164d9058eb8bb179d4eb36aad
prabi82 <prabikrishna@gmail.com>
2025-02-03 19:01:46 +0400

    Added simple test file

--- test.php
+++ test.php
@@ -1,11 +1,3 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-echo "<h1>PHP Test Page</h1>";
-echo "<p>PHP is working if you can see this message.</p>";
-echo "<p>PHP Version: " . phpversion() . "</p>";
-echo "<pre>";
-print_r($_SERVER);
-echo "</pre>";
+echo "Hello World";
 ?> 
